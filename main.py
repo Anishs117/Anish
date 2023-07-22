ceede49fcd96f1938c4833fc83790a5361b3fff0
Anishs117 <139550726+Anishs117@users.noreply.github.com>
2023-07-22 09:36:27 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,56 @@
-"""import math
-a = int(input("enter the 1st number"))
-b = int(input("enter the 2st number"))
-c = int(input("enter the 3st number"))
-if a==0:
-    print("error: 'a' cannot be zero")
-else:
-    discriminant= b**2 - 4*a*c
-    if discriminant<0:
-        print("error:root are complex.")
-    else:
-        root1 =(-b + math.sqrt(discriminant)) /(2*a)
-        root2 = (-b - math.sqrt(discriminant)) / (2 * a)
-        print("Root1:",root1)
-        print("Root2:", root2)
- #Q3
-sentence = input("Enter your sentence")
-letter_count=0
-digit_count=0
-for char in sentence:
-    if char.isalpha():
-        letter_count +=1
-    elif char.isdigit():
-        digit_count+=1
-print("letters",letter_count)
-print("letters",digit_count)"""
+ist=[]
+for i in range (2000,2500):
+    if (i%5==0 and i%8==0):
+        list.append(i)
+        print(i)
 #Q2
-sentence = input("entere a sentence")
-word= sentence.split()
-print (word)
-
-count1 = sentence.count("t")
-print (count1)
+a=int(input("enter a number"))`
+for i in range( 1,11):
+    j=(i*a)
+    print(i,"*",a,"=",j)
+#Q3
+a=input("enter a word")
+b= a.split()
+b.sort()
+print("the asscending order is",b)
+#Q4
+a=(input("enter a number"))
+b= a.split()
+b.sort()
+print("the second largest number is",b," ",b[-2])
+#Q5
+a=[1,2,3,4,5,6,7,8,9,10]
+evennum=[]
+oddnum=[]
+for i in a:
+    if (i%2 ==0 ):
+        evennum.append(i)
+    else:
+        oddnum.append(i)
+print(evennum,oddnum)
+#Q6
+b=[]
+User = input("enter a number ")
+b = User.split()
+n = b[::-1]
+print(n)
+#Q7
+b=[]
+for i in range(1,51):
+     if i%2!=0:
+        b.append(i)
+     else:
+         continue
+print(b)
+#Q8
+even=[]
+odd=[]
+n=[]
+user= (input("enter number"))
+l= user.split()
+for i in l:
+    if int(i)%2==0:
+        even.append(i)
+    elif int(i)%2!=0:
+        odd.append(i)
+print(even,odd)
